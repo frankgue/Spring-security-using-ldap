9845d793a5daf8dfb3a90591f7a7e0c61a5f7932
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-09-21 18:28:10 +0100

    Spring-security-using-ldap

--- src/main/java/com/gkfcsolution/springsecurityusingldap/api/config/SecurityConfig.java
+++ src/main/java/com/gkfcsolution/springsecurityusingldap/api/config/SecurityConfig.java
@@ -30,53 +30,37 @@ import java.util.List;
 @EnableMethodSecurity
 public class SecurityConfig {
 
- /*   @Bean
-    public LdapContextSource contextSource(){
+    @Bean
+    public LdapContextSource contextSource() {
         LdapContextSource contextSource = new LdapContextSource();
         contextSource.setUrl("ldap://localhost:8389");
         contextSource.setBase("dc=springframework,dc=org");
         contextSource.setUserDn("cn=admin,dc=springframework,dc=org");
         contextSource.setPassword("adminpassword");
+        contextSource.afterPropertiesSet();
         return contextSource;
-    }*/
-
-    /*  @Bean
-    public AuthenticationManager authenticationManager(LdapContextSource contextSource){
-        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
-        factory.setUserDnPatterns("uid={0},ou=people");
-        // Ici on dit : login avec uid -> on reconstruit le DN complet
-        return factory.createAuthenticationManager();
-    }*/
-
-    private final LdapAuthenticationProvider ldapAuthenticationProvider;
-
-    public SecurityConfig(LdapAuthenticationProvider ldapAuthenticationProvider) {
-        this.ldapAuthenticationProvider = ldapAuthenticationProvider;
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() {
-        // On force l’utilisation de ton provider custom
-        return new ProviderManager(List.of(ldapAuthenticationProvider));
+    public AuthenticationManager authenticationManager(LdapContextSource contextSource) {
+        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
+        factory.setUserDnPatterns("uid={0},ou=people"); // Recherche des users
+        return factory.createAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Si dans ton LDIF tu as {noop}user5pass, alors pas de hash
-        return NoOpPasswordEncoder.getInstance();
-//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance(); // adapté si {noop}userPassword dans LDIF
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf(AbstractHttpConfigurer::disable)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
-
         return http.build();
     }
 }
